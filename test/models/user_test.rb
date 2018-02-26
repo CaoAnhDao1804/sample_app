85c20f4e4ba136afbe1ad96d00aa11f92a29103c
CaoAnhDao1804 <caoanhdao18041996@gmail.com>
2018-02-26 16:16:48 +0700

    Finish chapter 6

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,9 @@
-require 'test_helper'
+require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+      password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -10,7 +11,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should be present" do
-    @user.name="    "
+    @user.name = "    "
     assert_not @user.valid?
   end
 
@@ -20,18 +21,18 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "name should not be too long" do
-    @user.name = "a"*51
+    @user.name = "a" * 51
     assert_not @user.valid
   end
 
   test "email should not be too long" do
-    @user.email = "a"*244 + "@example.com"
+    @user.email = "a" * 244 + "@example.com"
     assert_not @user.valid
   end
 
   test "email validation should accept valid addresses" do
-    valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                         first.last@foo.jp alice+bob@baz.cn]
+    valid_addresses = %w([user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn])
     valid_addresses.each do |valid_address|
       @user.email = valid_address
       assert @user.valid?, "#{valid_address.inspect} should be valid"
@@ -50,5 +51,15 @@ class UserTest < ActiveSupport::TestCase
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
