071f7b8cb2814216fd3a88eabfe894dd2e70869e
CaoAnhDao1804 <caoanhdao18041996@gmail.com>
2018-03-06 16:23:36 +0700

    Finish c10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:session][:password])
       log_in @user
       params[:session][:remember_me] == Settings.session_controller.remember_me ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = t ".combination"
       render :new
